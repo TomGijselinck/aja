96b0f97ea2eac8a44db55c9aa5e3eca00af035bc
Tom Gijselinck <tomgijselinck@gmail.com>
2017-09-16 14:44:30 +0200

    Add profile image to user

--- Backend/aja-backend/db/schema.rb
+++ Backend/aja-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916103652) do
+ActiveRecord::Schema.define(version: 20170916120821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170916103652) do
     t.datetime "updated_at", null: false
     t.string "name", limit: 100
     t.string "device_token"
+    t.string "image_url"
   end
 
 end
