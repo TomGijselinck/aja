cca2d25e1965d2b013550804d8b4413ed66d1e82
Bernd Schrooten <schrootenbernd@gmail.com>
2017-09-16 18:10:16 +0200

    Work on navigation

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import { TabNavigator } from 'react-navigation'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
 
-import reducers from './reducers'
+import Navigator from './navigator'
 import mySaga from './sagas'
-import Challenges from './components/Challenges'
-import Friends from './components/Friends'
-import CameraScreen from './components/CameraScreen'
-import PostChallengeScreen from './components/PostChallengeScreen'
+import reducers from './reducers'
 
 const sagaMiddleware = createSagaMiddleware()
 
-const TabNav = TabNavigator({
-  Home: {
-    screen: Friends,
-  },
-  Notifications: {
-    screen: Challenges,
-  },
-  CameraScreen: {
-    screen: CameraScreen,
-  },
-  PostChallenge: {
-    screen: PostChallengeScreen,
-  }
-}, {
-  tabBarComponent: () => null,
-  tabBarPosition: 'bottom',
-  animationEnabled: true,
-  swipeEnabled: true,
-  tabBarOptions: {
-    activeTintColor: '#e91e63',
-  },
-});
 
 const store = createStore(
   reducers,
@@ -48,7 +22,7 @@ store.dispatch({type: 'INIT'})
 export default function MyApp () {
   return (
     <Provider store={store}>
-      <TabNav/>
+      <Navigator/>
     </Provider>
   )
 }
--- Frontend/reducers.js
+++ Frontend/reducers.js
@@ -1,39 +1,59 @@
-import { GET_CHALLENGE_LIST_RESULT, GET_FRIENDS_LIST_RESULT, IMG_TAKEN } from './actions'
+import { GET_CHALLENGE_LIST_RESULT, GET_FRIENDS_LIST_RESULT, IMG_TAKEN, POST_CHALLENGE_RESULT } from './actions'
+import {Navigator} from './navigator'
 
 const initialState = {
+  nav: Navigator.router.getActionForPathAndParams('Login'),
   user: 1,
   currentImage: null,
   friends: [],
   challenges: [],
 }
 
+function navReducer (state, action) {
+  const newState = Navigator.router.getStateForAction(action, state)
+
+  return (newState || state)
+}
+
 export default function reducer (state = initialState, action) {
+  state = {
+    ...state,
+    nav: navReducer(state.nav, action)
+  }
+  console.log(action)
   switch (action.type) {
     case IMG_TAKEN:
       return {
         ...state,
         currentImage: action.payload.image,
       }
-    case GET_CHALLENGE_LIST_RESULT: {
+    case GET_CHALLENGE_LIST_RESULT:
       if (action.payload.error) {
-
+        return state
       } else {
         return {
           ...state,
           challenges: action.payload.body
         }
       }
-    }
-    case GET_FRIENDS_LIST_RESULT: {
+    case GET_FRIENDS_LIST_RESULT:
       if (action.payload.error) {
-
+        return state
       } else {
         return {
           ...state,
           friends: action.payload.body
         }
       }
-    }
+    case POST_CHALLENGE_RESULT:
+      if (!action.payload.error) {
+        return {
+          ...state,
+          currentImage: null,
+        }
+      } else {
+        return state
+      }
     default:
       return state
   }
