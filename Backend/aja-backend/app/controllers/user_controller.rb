cf810d9508e7f68181732085a19eb66f07214eee
Tom Gijselinck <tomgijselinck@gmail.com>
2017-09-16 16:22:40 +0200

    Add scores to friends list

--- Backend/aja-backend/app/controllers/user_controller.rb
+++ Backend/aja-backend/app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   def show
-    user = User.find(params[:id]).to_json
+    user = User.find(params[:id])
     render json: user
   end
 
@@ -15,8 +15,10 @@ class UserController < ApplicationController
   end
 
   def friends
-    friends = User.find(params[:id]).friends
-    render json: friends
+    user = User.find(params[:id])
+    friends = user.friends
+    friends.each do |friend| check_for_challenge_expiration(friend) end
+    render json: generate_friends_scores_json(friends, user)
   end
 
   def challenges
@@ -33,4 +35,27 @@ class UserController < ApplicationController
     user.save
     render json: { head: :ok }
   end
+
+  private
+
+    def check_for_challenge_expiration(user)
+      user.challenges_received.each do |challenge|
+        if (Time.now - challenge.time_created_at) > 24.hour
+          challenge.state = 'failed'
+          challenge.save
+        end
+      end
+    end
+
+    def generate_friends_scores_json(friends, user)
+      friends_json = []
+      friends.each do |friend|
+        friend_details = {}
+        friend_details[:id] = friend.id
+        friend_details[:name] = friend.name
+        friend_details[:score] = friend.score - user.score
+        friends_json.append friend_details
+      end
+      friends_json
+    end
 end
--- Backend/aja-backend/app/models/user.rb
+++ Backend/aja-backend/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
   has_many :challenges_received, class_name: 'Challenge', foreign_key: 'receiver_id'
   has_many :challenges_send, class_name: 'Challenge', foreign_key: 'sender_id'
+
+  def score
+    self.challenges_received.where(state: 'closed').size
+  end
 end
