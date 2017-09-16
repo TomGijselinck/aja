81e2b58e06a4b5b4389ed1853ef9c09baf97db35
Bernd Schrooten <schrootenbernd@gmail.com>
2017-09-16 16:47:16 +0200

    Add roboto fonts

--- Frontend/components/Header.js
+++ Frontend/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { List, ListItem, Avatar } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/MaterialIcons';
-const myIcon = (<Icon name="trending up" size={30} color="#900" />)
+import commonStyles from '../styles'
 
 
 
 export default function Header({children, title}) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
+      <Text style={[commonStyles.fontBold, styles.title]}>{title}</Text>
     </View>
   )
 }
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 10,
     fontSize: 30,
-    fontWeight: 'bold',
+    // fontWeight: 'bold',
     color: 'white',
   },
 
---
+++ Frontend/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export default styles = StyleSheet.create({
+  fontRegular: {
+    fontFamily: 'RobotoSlab-Regular'
+  },
+  fontBold: {
+    fontFamily: 'RobotoSlab-Bold'
+  },
+})
