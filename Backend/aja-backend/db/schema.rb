8bd661d3316dec0ad68ea4def454b7e230fc9cc4
Tom Gijselinck <tomgijselinck@gmail.com>
2017-09-16 09:33:08 +0200

    Create friendships

--- Backend/aja-backend/db/schema.rb
+++ Backend/aja-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916015519) do
+ActiveRecord::Schema.define(version: 20170916071627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20170916015519) do
     t.string "state"
     t.index ["receiver_id"], name: "index_challenges_on_receiver_id"
     t.index ["sender_id"], name: "index_challenges_on_sender_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
